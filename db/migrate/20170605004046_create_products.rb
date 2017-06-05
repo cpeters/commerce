ea6b3539b3438bdc013c42507d7cf29d2d304cc3
Chris Peters <chrispeters.psu@gmail.com>
2017-06-04 20:41:40 -0400

    Generate product model

---
+++ db/migrate/20170605004046_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
