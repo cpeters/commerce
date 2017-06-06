702220121abd7cc27f255a97684395d784f4e539
Chris Peters <chrispeters.psu@gmail.com>
2017-06-06 08:49:13 -0400

    Add associations and validations to product and rate_plan

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :rate_plans, dependent: :destroy
+
+  validates :title, :description, :start_date, :end_date, presence: true
+  validates :title, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
+
 end
--- app/models/rate_plan.rb
+++ app/models/rate_plan.rb
@@ -1,3 +1,10 @@
 class RatePlan < ApplicationRecord
+  RECURRENCES = %w(one-time monthly quarterly annually).freeze
+
   belongs_to :product
+
+  validates :title, :description, :start_date, :end_date, :recurrence, :price, presence: true
+  validates :title, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
+  validates :recurrence, inclusion: { in: RECURRENCES }
+
 end
