173f3673736ad74c2a31ece771ab8c6cb95c3fd3
Chris Peters <chrispeters.psu@gmail.com>
2017-06-06 09:15:16 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :show] do
+        resources :rate_plans, only: [:index]
+      end
+    end
+  end
+
   namespace :admin do
     resources :products
     resources :rate_plans
-
     root to: "products#index"
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Administrate expects these routes that aren't part of resources when in api mode
+  namespace :admin, as: '' do
+    get '/products/new', to: 'products#new', as: 'new_admin_product'
+    get '/products/:id/edit', to: 'products#edit', as: 'edit_admin_product'
+
+    get '/rate_plans/new', to: 'rate_plans#new', as: 'new_admin_rate_plan'
+    get '/rate_plans/:id/edit', to: 'rate_plans#edit', as: 'edit_admin_rate_plan'
+  end
+
 end
